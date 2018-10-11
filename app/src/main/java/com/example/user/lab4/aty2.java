8dbcf83b512438c370ccea325b38f7421dd27987
m5614m5614 <m5614m5614ss@gmail.com>
2018-10-11 15:57:39 +0800

    lab4

---
+++ app/src/main/java/com/example/user/lab4/aty2.java
@@ -0,0 +1,18 @@
+package com.example.user.lab4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Editable;
+
+public class aty2 extends AppCompatActivity {
+    sent sent_btn;
+    String suger ="無糖";
+    EditText set_drink;
+    String ice_opt="微冰";
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aty2);
+    }
+
+}
